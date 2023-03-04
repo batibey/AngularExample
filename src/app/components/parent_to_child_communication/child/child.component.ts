23cf9ca33e19bd906b4988e216495e3840e30a80
Mustafa Batı <mustafabati9@gmail.com>
2023-03-04 09:41:21 +0300

    TEST

--- src/app/components/parent_to_child_communication/child/child.component.ts
+++ src/app/components/parent_to_child_communication/child/child.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-child',
   template: `
-  <p>child works!!</p>
+  <p>child works!! TEST</p>
   {{childData}}
   `
 })
